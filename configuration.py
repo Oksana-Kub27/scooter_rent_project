e221e6bf8e6ebde11cacef40127b438607db328a
Oksana Kublikova <to.oks@yandex.ru>
2025-03-20 00:55:06 +0300

    Обновлен URL в configuration.py

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-BASE_URL = "https://d29212ff-851c-40a8-8aa3-193c38ff5068.serverhub.praktikum-services.ru"
+BASE_URL = "https://ac8cbd84-a63d-48ab-8f0c-957faf8d6918.serverhub.praktikum-services.ru"
 CREATE_ORDER_URL = f"{BASE_URL}/api/v1/orders"
 GET_ORDER_BY_TRACK_URL = f"{BASE_URL}/api/v1/orders/track"
